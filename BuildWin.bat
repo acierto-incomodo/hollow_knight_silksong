70298d48902bb3dbdc40bdd626b0e85ad25559ac
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-01-01 13:03:38 +0100

    Act 7

--- BuildWin.bat
+++ BuildWin.bat
@@ -2,4 +2,4 @@ Clear.bat
 cp main.py launcher_win.py
 python -m PyInstaller --onefile --windowed --noconsole --icon=hollow_knight_silksong.ico launcher_win.py
 python -m PyInstaller --onefile --windowed --noconsole --icon=hollow_knight_silksong.ico installer_updater.py
-echo 1.0.1 > version_win_launcher.txt
+echo 1.0.2 > version_win_launcher.txt
